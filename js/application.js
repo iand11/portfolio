46ac186274bb98ade20ffe8498ea54a26862caaa
iand11 <ian.driscoll.o@gmail.com>
2017-06-23 19:27:20 -0700

    Add fade in and delay to titles on hero page

--- js/application.js
+++ js/application.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  $(".hero-font").hide();
+  $(".1").fadeIn(1800);
+  $(".2").delay(800).fadeIn(1500);
+  $(".3").delay(1600).fadeIn(1500);
+  $(".4").delay(2400).fadeIn(1500);
 });
